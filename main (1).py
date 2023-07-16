eac0fbd562f6f9e2224797464a016fffccec28f2
Brandon <86905571+Wingified@users.noreply.github.com>
2023-07-15 21:30:39 -0400

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+agetil = 90 - int(age)
+days = agetil * 365
+weeks = agetil * 52
+months = agetil * 12
+print(f"You have {days} days, {weeks} weeks, and {months} left. Count your days.")
+
+
+
+
+
+
